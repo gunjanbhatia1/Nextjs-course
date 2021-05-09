874139e7d229c8e1842522f18c5e20b611f1ecd6
GunjanBhat1a <gunjan.bhatia@thoughtworks.com>
2021-05-09 13:43:52 +0530

    ADD routing

---
+++ routing-demo/src/App.js
@@ -0,0 +1,30 @@
+import { Route, Switch } from "react-router-dom";
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupPage from "./pages/NewMeetup";
+import FavouritePage from "./pages/Favourite";
+
+function App() {
+  return (
+    <div>
+      <Switch>
+        <Route path="/" exact>
+          <AllMeetupsPage />
+        </Route>
+        <Route path="/new-meetup">
+          <NewMeetupPage />
+        </Route>
+        <Route path="favourites">
+          <FavouritePage />
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
+
+//Route define different paths and url of the page which should be loaded for these different paths
+//path is url after domain after localhost:3000 or in real my-page.com
+//switch component tell that only one of these should be active
+//it start looking for match once got it will stop so so after matching / it won't go further so we should mention accordingly
+//for this we can also use exact instead
---
+++ routing-demo/src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+
+import "./index.css";
+import App from "./App";
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
+
+//for wrapping app with other component we need BrowserRouter
+//now we have initialised this router package and it will watch our urls
